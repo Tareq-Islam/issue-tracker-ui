aeeac17d5ed7c933afe8d6c97c5ee27e0c6cd461
Tareq Islam <tareqislam42@gmail.com>
2024-02-19 20:41:22 +0600

    fix

--- issue-tracker/@core/core/api/common/api-response.model.ts
+++ issue-tracker/@core/core/api/common/api-response.model.ts
@@ -1,8 +1,3 @@
-export enum KmsPermissionType {
-  NoPermission,
-  CustodianUser,
-  KeyRequestUser,
-}
 export class ApiResponse<T> {
   statusCode!: number;
   message!: string;
@@ -18,70 +13,4 @@ export class ApiResponseCount<T> {
 
 export interface Adapter<T> {
   adapt(item: T): T;
-}
-
-export interface DoorUnlockStrategyJson {
-  isWebUnlockActive: boolean;
-  isCardUnlockActive: boolean;
-  isSmsUnlockActive: boolean;
-  isMobileAppUnlockActive: boolean;
-}
-
-export interface GenericAlertNotificationJson {
-  email: boolean;
-  sms: boolean;
-  pushNotification: boolean;
-}
-
-export interface AuditLogUser {
-  creatorUserId: number;
-  lastModifierUserId: number;
-}
-
-export interface DoorCurrentStatus {
-  deviceId: number;
-  isActive: boolean;
-  doorStatus: number;
-  lockStatus: number;
-  indoorReader: number;
-  isIndoorReaderEnable: boolean;
-  outDoorReader: number;
-  isOutdoorReaderEnable: boolean;
-  isOnline: boolean;
-  rssi: number;
-  lastRssiTime: Date;
-  lastCommunicationTime: Date;
-  isMemoryFault: boolean;
-  lastSyncTime: Date;
-  customerName: string;
-  mobileOperatorInitialNumber: string;
-}
-
-export interface OrgDoorUnlockStrategy {
-  isWebUnlockActive: boolean;
-  isCardUnlockActive: boolean;
-  isSmsUnlockActive: boolean;
-  isMobileAppUnlockActive: boolean;
-}
-
-export interface CustomerUserInfo {
-  name: string;
-  organization: string;
-  email: string;
-  mobileNumber: string;
-  cardNumber: string;
-  permission: string;
-  loginStatus: boolean;
-  doorUnlockStrategy: OrgDoorUnlockStrategy;
-  status: boolean;
-}
-
-export interface AssetAlias {
-  org: string;
-  aliasName: string;
-}
-
-export enum DeviceModelType {
-  SmartLock = 1,
-  BluetoothLock = 2,
 }
